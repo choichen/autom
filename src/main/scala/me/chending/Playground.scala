e2d77fb613584a940dd397fca56aaf2b08468890
U-TENCENT\choichen <choichen@choichen-PC4.tencent.com>
2015-05-11 19:19:17 +0800

    删除无关代码

--- src/main/scala/me/chending/Playground.scala
+++ src/main/scala/me/chending/Playground.scala
@@ -1,19 +1,10 @@
 package me.chending
 
 import me.chending.ini.INIParser
-import scala.actors._, Actor._
 
 object Playground extends App {
     val parser: INIParser = new INIParser(InputHelper.from(getClass.getResource("/test.ini")))
     parser.start()
 
     println(parser.getValueMap)
-
-    val badActor = actor {
-        receive {
-            case msg => println(msg)
-        }
-    }
-
-    badActor ! "Hi!"
 }
